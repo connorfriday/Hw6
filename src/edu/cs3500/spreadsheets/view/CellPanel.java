7cbaf1697f209b54efd9001b93d68308974f0faf
connorfriday <cfbfriday@gmail.com>
2019-11-18 12:09:09 -0500

    Added scroll bars.

---
+++ src/edu/cs3500/spreadsheets/view/CellPanel.java
@@ -0,0 +1,12 @@
+package edu.cs3500.spreadsheets.view;
+
+public class CellPanel extends JPanel {
+
+  CellPanel() {
+    super();
+    GridBagLayout layout = new GridBagLayout();
+    JPanel grid = new JPanel(layout);
+  }
+
+
+}
